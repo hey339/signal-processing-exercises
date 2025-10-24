e55f41f828c3e8c0c7f79e486d827a2c46eca9cc
hey339 <pcaya0559@gmail.com>
2025-10-25 01:56:44 +0200

    Add files via upload

---
+++ exo3s1.m
@@ -0,0 +1,8 @@
+Fe = 44100;                     
+t = 0:1/Fe:2;        
+f0 = 400;
+f1 = 500;
+f2 = 1000;
+% Construction du signal complexe
+x = sin(2*pi*f0*t) + 0.7*sin(2*pi*f1*t) + 0.7*sin(2*pi*f2*t);
+sound(x, Fe);  

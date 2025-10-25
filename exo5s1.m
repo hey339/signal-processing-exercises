464b4e664d390a04ad410ae6cf82ef9eb33528aa
hey339 <pcaya0559@gmail.com>
2025-10-25 15:14:18 +0200

    Fix formatting and re-add calculations in exo5s1.m

--- exo5s1.m
+++ exo5s1.m
@@ -3,7 +3,7 @@ U = (x >= 0);
 V = exp(-0.3*abs(x));   
 
 Y_add  = U + V;          % Addition
-Y_mult = 0.6*U + 0.4*V;  % Combinaison linéaire
+Y_mult = 0.6*U + 0.4*V;  
 Y_trunc = U .* (abs(x)<=5); % Troncature
 Y_rev = fliplr(U);       % Reverse
 
@@ -11,4 +11,5 @@ stem(x, Y_add); title('Addition');
 figure, stem(x, Y_mult); title('Multiplication');
 figure, stem(x, Y_trunc); title('Troncature');
 figure, stem(x, Y_rev); title('Reverse');
+
 
